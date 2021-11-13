8d795e36850e4b3ce7ec9b3882881f113564a487
Daniel Hinz <dhinz1@kent.edu>
2021-11-12 21:18:22 -0500

    updated db attribute values

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from sqlalchemy.sql.expression import false, true
 from datetime import datetime
 
 import sqlalchemy as db
+import math, re
 
 app = Flask(__name__)
 
@@ -22,22 +23,22 @@ Session(app)
 ## Database Initialization
 
 class Person(db.Model):
-    id           = db.Column(db.Integer, primary_key = True)
+    id           = db.Column(db.Integer,  primary_key = True)
     fullname     = db.Column(db.String(200), nullable = True)
-    username     = deferred(db.Column(db.String(200)))
+    username     = db.Column(db.String(200), nullable = True)
     address      = db.Column(db.String(200), nullable = True)
-    age          = db.Column(db.Integer, nullable = True)
-    salary       = db.Column(db.Integer, nullable = True)
+    age          = db.Column(db.String(200), nullable = True)
+    salary       = db.Column(db.String(200), nullable = True)
     job_title    = db.Column(db.String(200), nullable = True)
-    password     = deferred(db.Column(db.String(200)))
+    password     = db.Column(db.String(200), nullable = True)
     date_created = db.Column(db.DateTime, default = datetime.utcnow)    
 
     def __init__(self,fullname,username,address,age,salary,job_title,password):
-        self.fullname   = fullname
+        self.fullname   = "*"
         self.username   = username
-        self.address    = address
-        self.age        = age
-        self.salary     = salary
+        self.address    = address[len(address)-6:len(address)-2]
+        self.age        = "> 45" if int(age) > 45 else "<= 45"
+        self.salary     = str(math.floor(int(salary) / 10000) * 10000) + " - " + str((math.floor(int(salary) / 10000) + 1) * 10000)
         self.job_title  = job_title
         self.password   = password
             
@@ -75,13 +76,13 @@ def login():
 @app.route("/signup", methods = ['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        new_fullname    = request.form['fullname']
-        new_username    = request.form['username']
-        new_address     = request.form['address']
-        new_age         = request.form['age']
-        new_salary      = request.form['salary']
-        new_job_title   = request.form['job_title']
-        new_password    = request.form['password']
+        new_fullname  = request.form['fullname']
+        new_username  = request.form['username']
+        new_address   = request.form['address']
+        new_age       = request.form['age']
+        new_salary    = request.form['salary']
+        new_job_title = request.form['job_title']
+        new_password  = request.form['password']
 
         # Check all fields are filled out
         if (not new_fullname  or not new_username  or not new_address   or not new_age
@@ -92,6 +93,7 @@ def signup():
         if db.session.query(db.exists().where(Person.username == new_username)).scalar():
             return render_template('signup.html', duplicate = True)
 
+        # Add user to database
         user = Person(new_fullname, new_username, new_address, new_age, new_salary, new_job_title, new_password)
         db.session.add(user)
         db.session.commit()
@@ -115,33 +117,30 @@ def signout():
 def home():
     if not session.get("name"):
         return redirect(url_for("login"))
-
-    ###############################################
-    ## Implement l-diversity 
     
-        ## Hide username and password column
-
-        ## Update name column
-
-        ## Limit address
-
-        ## Update ages
-
-        ## Update salary
-
-        ## Update element
-        # db.session.commit()
-    
-    ## Return Updated Table
     return render_template('index.html', 
                             headings = Person.__table__.columns.keys(), 
                             people   = Person.query.all())
 
 
 ###############################################
+## Populate Database
+def populate():
+    with open('dummy.txt', 'r') as file:
+        for line in file:
+            values = re.split(r'\t+', line.rstrip('\t'))
+            values[6] = values[6][:len(values[6])-1]
+            user = Person(values[0], values[1], values[2], values[3], values[4], values[5], values[6])
+            db.session.add(user)
+        
+        db.session.commit()            
+
+
+###############################################
 ## General
 if __name__ == "__main__":
-    db.create_all()
+    # db.create_all()
+    # populate()
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run(debug=True, host='0.0.0.0')
